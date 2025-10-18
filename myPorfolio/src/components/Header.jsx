44f5ecb1261118eee3b855b25d55e35ea5bf6088
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-10-18 22:16:33 +0100

    first commit

---
+++ myPorfolio/src/components/Header.jsx
@@ -0,0 +1,27 @@
+
+function Header(){
+
+    return(
+        <header>
+          <nav>
+                <ul>
+                    <li><a href ="">Home</a></li>
+                    
+
+                </ul>
+
+
+
+          </nav>
+
+
+
+        </header>
+
+    )
+}
+
+
+
+
+export default Header;
---
+++ myPorfolio/src/components/Tphils.jsx
@@ -0,0 +1,24 @@
+
+
+
+function Tphils(){
+
+ return (
+
+    <section>
+
+        <div>
+        <img className="Picture" src={profilePicture} alt="profile picture"/>
+
+        </div>
+
+
+
+    </section>
+
+
+ );
+
+}
+
+export default Tphils
